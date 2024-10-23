d62c0183aacc0cbcc4a2d5a4cba85ec17cbb1454
leone-nyaga <leonenyaga@gmail.com>
2024-10-23 19:56:32 +0300

    ALMOST A CIRCLE

--- models/rectangle.py
+++ models/rectangle.py
@@ -75,4 +75,19 @@ class Rectangle(Base):
 
         """Returns area of Rectangle"""
         def area(self):
-            return (self.__width * self.__height)
+            return self.__width * self.__height
+
+        def display(self):
+            """Prints rectangle with #"""
+            for _ in range(self.height):
+                print('#' * self.width)
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(4, 6)
+    r1.display()
+
+    print("---")
+
+    r1 = Rectangle(2, 2)
+    r1.display()
