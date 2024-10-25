1c8937f7a394bf1ba960342cde7746082fc72480
leone-nyaga <leonenyaga@gmail.com>
2024-10-25 11:29:49 +0300

    ALMOST A CIRCLE

--- models/square.py
+++ models/square.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
+"""This module contains the Square class."""
 
 from rectangle import Rectangle
 
+
 class Square(Rectangle):
+    """Represents the Square class that inherits from the Rectangle class."""
+
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(x, y,width, height, id)
+        """
+        Initializes Square instance.
+
+        :param size: The size of the square (width and height).
+        :param x: The x coordinate (default is zero).
+        :param y: The y coordinate (default is zero).
+        :param id: Optional id for the instance.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Overrides the __str__ method to return a statement."""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
