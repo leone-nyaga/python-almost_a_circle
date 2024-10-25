859c11aca263b336865bd2a7e30573148979e8fb
leone-nyaga <leonenyaga@gmail.com>
2024-10-25 11:40:32 +0300

    ALMOST A CIRCLE

--- models/square.py
+++ models/square.py
@@ -18,6 +18,17 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        """Setter for size."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Overrides the __str__ method to return a statement."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
