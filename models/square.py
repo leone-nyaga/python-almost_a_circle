316019100f94d51a6bf2bb44106c159d298aac2b
leone-nyaga <leonenyaga@gmail.com>
2024-10-25 11:45:08 +0300

    ALMOST A CIRCLE

--- models/square.py
+++ models/square.py
@@ -29,6 +29,36 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """
+        Updates the Square attributes based on provided arguments.
+
+        :param args: Variable length argument list (ignored if not empty).
+        :param kwargs: Keyword arguments for attribute updates.
+        """
+        if args:
+            # Unpack args into attributes
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]  # This uses the size setter
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+            return  # Return after processing args
+
+        for key, value in kwargs.items():
+            if key == 'id':
+                self.id = value
+            elif key == 'size':
+                self.size = value  # This uses the size setter
+            elif key == 'x':
+                self.x = value
+            elif key == 'y':
+                self.y = value
+
+
     def __str__(self):
         """Overrides the __str__ method to return a statement."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
